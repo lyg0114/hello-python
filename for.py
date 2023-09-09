71395f012d3138859684c4a2261724f475926aab
lyg0114 <whdnseowkd@naver.com>
2023-09-09 09:33:39 +0200

    Add : test for and if

--- for.py
+++ for.py
@@ -1,3 +1,29 @@
+
 words = ['cat', 'window', 'defenestrate']
 for w in words:
     print(w, len(w))
+
+print("########################")
+
+# Create a sample collection
+users = {
+    'Hans': 'active',
+    'Éléonore': 'inactive',
+    '景太郎': 'active'
+}
+
+for user in users:
+    print(user)
+
+print("########################")
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
